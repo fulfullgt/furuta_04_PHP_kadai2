5b0ecd487d164cd6e3a85f46c8601a8a557f50b4
fulfullgt <fulfullgt@gmail.com>
2022-01-23 22:24:34 +0900

    first commit

---
+++ insert.php
@@ -0,0 +1,43 @@
+<?php
+// 1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+
+$bookname = $_POST['bookname'];
+$bookurl = $_POST['bookurl'];
+$bookcomment = $_POST['bookcomment'];
+
+// 2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=fulfull_book_db;charset=utf8;host=mysql57.fulfull.sakura.ne.jp','fulfull','Furu-1549');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+
+// ３．SQL文を用意(データ登録：INSERT)
+$stmt = $pdo->prepare(
+  "INSERT INTO gs_bm_table(uni,bookname,bookurl,bookcomment,date)
+  VALUES( NULL, :bookname, :bookurl, :bookcomment, sysdate() )"
+);
+
+// 4. バインド変数を用意
+$stmt->bindValue('bookname', $bookname, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue('bookurl', $bookurl, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue('bookcomment', $bookcomment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+// 5. 実行
+$status = $stmt->execute();
+
+
+// 6．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMassage:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header('location: index.php');
+}
+?>
